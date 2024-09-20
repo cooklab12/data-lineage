92b13cee4ab5a7e5ae81cf487b7eb97a40289486
Ravi <cooklab12@gmail.com>
2024-09-19 23:18:43 -0400

    json output

--- ParquetReaderExample.java
+++ ParquetReaderExample.java
@@ -1,3 +1,5 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.parquet.column.page.PageReadStore;
@@ -12,45 +14,25 @@ import org.apache.parquet.schema.MessageType;
 import org.apache.parquet.schema.Type;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ParquetReaderExample {
     public static void main(String[] args) throws IOException {
-        String parquetFilePath = "/path/to/your/file.parquet";
+        // Change this to your local file path
+        String localFilePath = "/path/to/your/local/file.parquet";
         
         Configuration conf = new Configuration();
-        Path path = new Path(parquetFilePath);
+        Path path = new Path(localFilePath);
+        
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<ObjectNode> jsonRows = new ArrayList<>();
         
         try (ParquetFileReader reader = ParquetFileReader.open(HadoopInputFile.fromPath(path, conf))) {
             MessageType schema = reader.getFooter().getFileMetaData().getSchema();
             List<Type> fields = schema.getFields();
             
-            System.out.println("Column Headers:");
-            for (Type field : fields) {
-                System.out.println(field.getName());
-            }
-            
-            System.out.println("\nFirst 10 rows of data:");
             PageReadStore pages;
-            int rowCount = 0;
-            
-            while ((pages = reader.readNextRowGroup()) != null && rowCount < 10) {
-                long rows = pages.getRowCount();
-                MessageColumnIO columnIO = new ColumnIOFactory().getColumnIO(schema);
-                RecordReader<SimpleGroup> recordReader = columnIO.getRecordReader(pages, new GroupRecordConverter(schema));
-                
-                for (int i = 0; i < rows && rowCount < 10; i++) {
-                    SimpleGroup simpleGroup = recordReader.read();
-                    System.out.println(simpleGroup);
-                    rowCount++;
-                }
-            }
-            
-            // Example of filtering using two columns
-            System.out.println("\nFiltered results (example):");
-            reader.close();
-            reader = ParquetFileReader.open(HadoopInputFile.fromPath(path, conf));
-            
             while ((pages = reader.readNextRowGroup()) != null) {
                 long rows = pages.getRowCount();
                 MessageColumnIO columnIO = new ColumnIOFactory().getColumnIO(schema);
@@ -58,14 +40,55 @@ public class ParquetReaderExample {
                 
                 for (int i = 0; i < rows; i++) {
                     SimpleGroup simpleGroup = recordReader.read();
-                    // Replace "column1" and "column2" with your actual column names
-                    // Adjust the conditions as needed
-                    if (simpleGroup.getString("column1", 0).equals("desiredValue1") &&
-                        simpleGroup.getInteger("column2", 0) > 100) {
-                        System.out.println(simpleGroup);
+                    ObjectNode jsonRow = objectMapper.createObjectNode();
+                    
+                    for (int j = 0; j < fields.size(); j++) {
+                        String fieldName = fields.get(j).getName();
+                        Type.Repetition repetition = fields.get(j).getRepetition();
+                        
+                        if (repetition == Type.Repetition.REPEATED) {
+                            int valueCount = simpleGroup.getFieldRepetitionCount(j);
+                            List<Object> values = new ArrayList<>();
+                            for (int k = 0; k < valueCount; k++) {
+                                values.add(getFieldValue(simpleGroup, j, k));
+                            }
+                            jsonRow.putPOJO(fieldName, values);
+                        } else {
+                            Object value = getFieldValue(simpleGroup, j, 0);
+                            if (value != null) {
+                                jsonRow.putPOJO(fieldName, value);
+                            }
+                        }
                     }
+                    
+                    jsonRows.add(jsonRow);
                 }
             }
+        }
+        
+        // Print all rows in JSON format
+        System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonRows));
+    }
+    
+    private static Object getFieldValue(SimpleGroup simpleGroup, int fieldIndex, int valueIndex) {
+        Type fieldType = simpleGroup.getType().getType(fieldIndex);
+        String typeName = fieldType.asPrimitiveType().getPrimitiveTypeName().name();
+        
+        switch (typeName) {
+            case "BINARY":
+                return simpleGroup.getString(fieldIndex, valueIndex);
+            case "INT32":
+                return simpleGroup.getInteger(fieldIndex, valueIndex);
+            case "INT64":
+                return simpleGroup.getLong(fieldIndex, valueIndex);
+            case "FLOAT":
+                return simpleGroup.getFloat(fieldIndex, valueIndex);
+            case "DOUBLE":
+                return simpleGroup.getDouble(fieldIndex, valueIndex);
+            case "BOOLEAN":
+                return simpleGroup.getBoolean(fieldIndex, valueIndex);
+            default:
+                return null;
         }
     }
 }
